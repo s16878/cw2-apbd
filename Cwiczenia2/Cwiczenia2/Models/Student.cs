160709eef79db614605ef554099d57d944b57a29
s16878 <s16878@LAB223-03>
2020-03-09 17:10:03 +0100

    p

---
+++ Cwiczenia2/Cwiczenia2/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cwiczenia2.Models
+{
+    public class Student
+    {
+        public string Email { get; set; }
+        public string Imie { get; set; }
+
+        private string _nazwisko;
+        public string Nazwisko
+        {
+            get { return _nazwisko;  }
+            set
+            {
+                if (value == null) throw new ArgumentException();
+                _nazwisko = value;
+            }
+        }
+    }
+}
---
+++ Cwiczenia2/Cwiczenia2/Program.cs
@@ -0,0 +1,46 @@
+﻿using Cwiczenia2.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Cwiczenia2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string path = @"Data\dane.csv";
+
+            //wczytywanie
+            var fi = new FileInfo(path);
+            using (var stream = new StreamReader(fi.OpenRead()))
+            {
+                string line = null;
+                while ((line = stream.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+
+            //XML
+
+            var list = new List<Student>();
+            var st = new Student
+            {
+                Imie = "Jan",
+                Nazwisko = "Kowalski",
+                Email = "kowalski@wp.pl"
+            };
+            list.Add(st);
+
+            FileStream writer = new FileStream(@"data.xml", FileMode.Create);
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Student>),
+                                       new XmlRootAttribute("uczelnia"));
+            serializer.Serialize(writer, list);
+  
+        }
+    }
+}
